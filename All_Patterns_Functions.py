03385c2eb5009f1a2fdbec3fb589b7e3dc61119e
powerhouse2121 <anmolwadhwa210@gmail.com>
2024-03-21 13:40:19 +0530

    First Commit

---
+++ All_Patterns_Functions.py
@@ -0,0 +1,142 @@
+from PyPDF2 import PdfReader 
+import re  
+
+class Pattern_REGEX:
+    
+    #test_string='L65900MH2016PLC284869 L72400GA2004PLC147094 U74120MH2012PLC230380 '
+    #CIN_Number Structure : 1-Listing Status(1-Character) : Listing in stock exchange then i will (L) else (U)
+    #2-Industry Code 5-digit
+    #3-State Code : 2-Alphabets
+    #4-Year Of InCorporation : 4-digits
+    #5-Type Of The Company : 3-characters
+    #6-Company Registration Number : 6-digits
+    #\b[UL]{1}[0-9]{5}[A-Z]{2}[0-9]{4}[A-Z]{3}[0-9]{6}
+    #In CIN_Pattern, i use list for state_code and company_type that i used in pattern using join method
+    #append the result in list result_cin and return the list and count for each page
+    #In this pattern,some state_codes are used according to pdf's CIN nos,actually they are not proper cin nos(country_codes : TZ)
+    
+    def CIN_Pattern(self,text,i):
+        try:
+            result_cin=[]
+            
+            state_code=['AN','AP','AD','AR','AS','BH','CH','CT','DN','DD','DL','GA','GJ','HR','HP','JK','JH','KA','KL','LD','MP','MH','MN','ME','MI','NL','OR','PN','PY','PB','RJ','SK','TN','TS','TR','TG','TZ','UP','UT','WB']
+            company_type=['FLC','FTC','GAP','GAT','GOI','NPL','PLC','PTC','SGC','ULL','ULT']
+            pattern_cin=re.compile(r"[UL]{1}[0-9]{5}(" + "|".join(state_code) + ")[0-9]{4}(" + "|".join(company_type) + ")[0-9]{6}")
+            CIN_CODES=re.findall(pattern_cin,text)
+            return CIN_CODES
+            cnt=0
+            for CIN in CIN_CODES:
+                result_cin.append(CIN.group())
+                cnt+=1
+            return result_cin
+    #        print(f"Count Of CIN in page{i+1} is {cnt}")
+            logging.info("Successfully Run CIN_Pattern")
+        except Exception as e:
+            logging.error(f"Error Occured In CIN_Pattern : {e}")
+
+    #In Mail_Pattern, I use findall() to find all the occurences of mail_pattern in the file
+    #append the result in list result_mail and return the list and count for each page
+    
+    def Mail_Pattern(self,text,i):
+        try:
+            #result_mail=[]
+            pattern_mail = r'[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}'
+            emails = re.findall(pattern_mail, text)
+            return emails
+##            cnt=0
+##            for email in emails:
+##                cnt+=1
+##                result_mail.append(email)
+##            #print(f"Count Of Mails in page{i+1} is {str(cnt)}")
+            #return result_mail
+            logging.info("Successfully Run Mail_Pattern")
+        except Exception as e:
+            logging.error(f"Error Occured In Mail_Pattern : {e}")
+
+    # random_text = """
+    #Here are some numbers: (+91)9876543210,+91 9876543210, 011-12345678, 9876543230, +91 98765 43240.
+    #But also consider these: 1234-567890, 98765-43220, 01234567890 (012)22222222, (012) 22222222.
+    #"""            
+    #In Phone_Pattern, checks all the mobile numbers or std code landline phone numbers and checked in the text
+    #append the result in list result_phone and return the list and count for each page
+    
+    def Phone_Pattern(self,text,i):
+        try:
+            #result_phone=[]
+            pattern_phone = re.compile(r'\b\(?\+91\)?[\s\S][6789]\d{9}\b|\b(?:\+91|0)?\s[6789]{1}\d{4}[-\s]\d{5}\b|\b[6789]{1}\d{9}|[0-9]{3}[-\s]\d{8}\b|\b\(?[0-9]{3}\)?\s?\d{8}\b')
+            phone=pattern_phone.findall(text)
+            return phone
+##            cnt=0
+##            for ph in phone:
+##                cnt+=1
+##                result_phone.append(ph)
+            #print(f"Count Of Phone_Nos in page{i+1} is {str(cnt)}")
+            #return result_phone
+            logging.info("Successfully Run Phone_Pattern")
+        except Exception as e:
+            logging.error(f"Error Occured In Phone_Pattern : {e}")
+
+    #In PAN_Pattern, check all the PAN nos in the text using pattern
+    #append the result in list result_pan and return the list and count for each page
+    
+    def PAN_Pattern(self,file_content,i):
+        try:
+            #result_pan=[]
+            pattern_pan=re.compile(r'[A-Z]{5}\d{4}[A-Z]')
+            Pan=pattern_pan.findall(file_content)
+            return Pan
+##            cnt=0
+##            for num in Pan:
+##                result_pan.append(num)
+##                cnt+=1
+            #print(f"Count Of PAN_Nos in page{i+1} is {str(cnt)}")
+            #return result_pan
+            logging.info("Successfully Run PAN_Pattern")
+        except Exception as e:
+            logging.error(f"Error Occured In PAN_Pattern : {e}")
+
+    #In Date_Pattern, using the date_pattern iterates all the dates from the text
+    #append the result in list result_date and return the list and count for each page
+            
+    def Date_Pattern(self,text,i):
+        try:
+            result_date=[]
+            pattern=re.compile(r'[0-9]{2}\/[0-9]{2}\/[0-9]{4}')
+
+            #pattern1=re.compile(r"\d\d\/(d\d)\/(\d\d\d\d)")
+            Dates=pattern.findall(pattern,text)
+            return Dates
+##            cnt=0
+##            for Date in Dates:
+##                result_date.append(Date)
+##                cnt+=1
+            #print(f"Count Of Dates in page{i+1} is {str(cnt)}")
+            #return result_date
+            logging.info("Successfully Run Date_Pattern")
+        except Exception as e:
+            logging.error(f"Error Occured In Date_Pattern : {e}")
+
+    #Retrieve all the websites from text using pattern
+    #append the result in list result_websites and return the list and count for each page
+    
+    def Website_Pattern(self,text,i):
+        try:
+            #result_website=[]
+            pattern=re.compile(r'(https?|http?:\/\/)?w{3}\.\S+\.[A-Za-z-_]{2,}')
+            websites = re.findall(pattern,text)
+            return websites
+##            cnt=0
+##            for website in websites:
+##                result_website.append(website.group())
+##                cnt+=1
+            #print(f"Count Of Websites in page{i+1} is {str(cnt)}")
+            #return result_website
+            logging.info("Successfully Run CIN_Pattern")
+            
+        except Exception as e:
+            logging.error(f"Error Occured In CIN_Pattern : {e}")
+            
+    
+                    
+
+
---
+++ Main_Extractor_PDF.py
@@ -0,0 +1,118 @@
+from All_Patterns_Functions import Pattern_REGEX
+from PyPDF2 import PdfReader 
+import json
+from selenium import webdriver
+import time
+import logging
+import logger_file
+import pandas as pd
+
+class regex_main_function:
+
+    #Call load_config function
+    def __init__(self,config_file="config_regex.json"):    
+        self.config=self.load_config(config_file)
+
+    #load_config(config_file) : It includes config_file to load     
+    def load_config(self,config_file):          
+        with open(config_file) as config_file:
+            return json.load(config_file)
+
+    #download_pdf() function : I use the selenium package to download PDF file using url
+    #and store in passed directory
+    #To download PDF,i use webdriver.get(url) of particular url
+
+    #Here i put sleep function,because it takes more than a second to download when we not use sleep() method then it will not download
+    #i split the strings with / because i want file_name then i iterate -1
+    #then i append to the list file_names
+    #Change default directory for downloads
+    #Retrieve base_urls from config_regex.json file
+        
+    def download_pdf(self):
+        try:
+            file_names=[]
+            options = webdriver.ChromeOptions()
+            options.add_experimental_option('prefs', {
+            "download.default_directory": "D:\\Python Files\\Python_New_REGEX", 
+            #"download.prompt_for_download": False,  #Do not download
+            #"download.directory_upgrade": True,    #To auto download the file
+            "plugins.always_open_pdf_externally": True #It will not show PDF directly in chrome 
+            })
+            
+            driver = webdriver.Chrome(options=options)
+            #pdf_url = 'https://assets.airtel.in/teams/simplycms/web/docs/Draft-Annual-Return-FY-2021-22.pdf'
+            for pdf_url in self.config["base_urls"]:
+                driver.get(pdf_url)
+                time.sleep(10)
+                files=pdf_url.split("/")
+                file=files[-1]
+                file_names.append(file)
+            return file_names
+    
+            logging.info("Successfully Downloaded All The Files")
+            
+        except Exception as e:
+            logging.error(f"Error in downloading : {e}")
+
+    #In main() function, Call to download_pdf() and get the result as a list of files and iterate one by one
+    #fisrtly i get empty lists for all the functions to append the returned result page-wise
+    #and display all the lists with their cnt for each page
+    #In all functions,they returns 2 values - matched_patterns and count of each page of matched pattern
+    #It returns in list within a tuple because i use unpacking method
+            
+    def main(self):
+        try:
+            files=self.download_pdf()
+            dict1={}
+            for file in files:
+                print(f"\n\n-----------------------------------------This is {file}-----------------------------------------")
+
+##                all_CIN=[]
+##                all_Mails=[]
+##                all_Phones=[]
+##                all_PANS=[]
+##                all_Dates=[]
+##                all_Websites=[]
+                
+                reader = PdfReader(file) 
+                no_of_pages=self.config["no_of_pages"]
+                for i in range(no_of_pages):
+                    print(f"\nPage No: {i+1}\n")
+                    page = reader.pages[i]   
+                    
+                    text = page.extract_text()
+
+                    dict1={
+                        "CIN_Nos":Pat.CIN_Pattern(text,i),        
+                        "Mail_IDs":Pat.Mail_Pattern(text,i),
+                        "Phone_Nos":Pat.Phone_Pattern(text,i),
+                        "PAN_Nos":Pat.PAN_Pattern(text,i),
+                        "All_Date":Pat.Date_Pattern(text,i),
+                        "All_Websites":Pat.Website_Pattern(text,i)}
+
+                    print(dict1)
+                    
+                #df=pd.DataFrame()
+##                print("\nAll CIN Nos : \n",all_CIN)
+##                #print("All CIN Count : \n",len(all_CIN))
+##            
+##                print("\nAll MailID's : \n",all_Mails)
+##                print("\nAll Phone Nos : \n",all_Phones)
+##                print("\nAll PAN Nos : \n",all_PANS)
+##                print("\nAll Dates : \n",all_Dates)
+##                print("\nAll Websites : \n",all_Websites)
+##                
+            logging.info(f"Successfully Run All {files}")                    
+        except Exception as e:
+            logging.error(f"Error Occured In {file} : {e}")
+                
+        
+if __name__=="__main__":
+    Pat=Pattern_REGEX()
+
+    main_class=regex_main_function()
+    main_class.main()
+    
+
+
+
